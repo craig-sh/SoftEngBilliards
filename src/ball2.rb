8640b2da9ccec1e73b61fe7ca7ab68835ecfea07
craig-sh <craig.henriques@mail.utoronto.ca>
2013-10-02 00:12:32 -0400

    Made smoother, fixed speed bug, commented out decelaration for now

--- src/ball2.rb
+++ src/ball2.rb
@@ -1,14 +1,14 @@
 require 'rubygems'
 require 'rubygame'
 
-TICK = 1
+STEP = 0.01
 LENGTH = 600
 WIDTH = 300
-BALL_RADIUS = 50
-MAX_TIME = 2000
+BALL_RADIUS = 20
+MAX_TIME = 20000
 VELOCITY = 5.0
 NUM_BALLS = 20
-WAIT = 0.012
+WAIT = STEP/1000
 #PI = Math::PI
 color_array = ["red","blue","cyan","pink","Silver","Gray","Crimson","Navy","Azure","Lime","Gold","Brown","Teal","Purple","YeLlow"]
 class Ball
@@ -23,15 +23,15 @@ class Ball
   end
 
   def move    
-    @x_speed = @x_speed * (1 -  1/250.0 )    
-    @y_speed = @y_speed * (1 -  1/250.0)
+    #@x_speed = @x_speed * 0.00001
+    #@y_speed = @y_speed * 0.00001
     #set new postions and prevent the ball from travelling of screen so they can be handled by collision detection
-    @x_pos += @x_speed #unless (@x_pos ) <= 0 || (@x_pos ) >= WIDTH
-    @y_pos += @y_speed #unless (@y_pos ) <= 0 || (@y_pos ) >= LENGTH
+    @x_pos += @x_speed *  STEP #unless (@x_pos ) <= 0 || (@x_pos ) >= WIDTH
+    @y_pos += @y_speed  * STEP#unless (@y_pos ) <= 0 || (@y_pos ) >= LENGTH
 
-    if @x_pos.abs <= 0.5 and @y_pos.abs <= 0.5
-      @x_pos = 0
-      @y_pos = 0
+    if @x_speed.abs <= 0.05 and @y_speed.abs <= 0.05
+      @x_speed = 0
+      @y_speed = 0
     end
    
   end
@@ -40,15 +40,9 @@ class Ball
     (((@x_pos - ball.x_pos )** 2)  + ((@y_pos - ball.y_pos )** 2))** 0.5
   end    
   def to_s
-      "#{@colour} = #{@x_pos},#{@y_pos} at #{@x_speed},#{@y_speed}"
-  end
-  def add_collsion(ball)
-    @collision = ball
-    ball.collision = self
-  end
-  def remove_collision(ball)
-    @collion = nil
-    ball.collision = nil if ball.collision == self
+      color = nil
+      color = @collision.colour unless collision == nil
+      "#{@colour} = #{@x_pos},#{@y_pos} at #{@x_speed},#{@y_speed}\t\t#{color}"
   end
 end
 #################DRAWING###################
@@ -68,20 +62,28 @@ class Drawer
       color = eval "Rubygame::Color[:#{ball.colour}]" #[ 0xc0, 0x80, 0x40]      
       @screen.draw_circle_s  center, radius, color
     end
-    sleep(WAIT)
+    #sleep(WAIT)
     @screen.flip
   end
 end
 #################################################
 #################FUNCTIONS################################
+def add_collision(ball1,ball2)
+  ball1.collision = ball2
+  ball2.collision = ball1
+end
+def remove_collision(ball1,ball2)
+  ball1.collision = nil
+  ball2.collision = nil
+end
 def sim_collision_with(ball1,ball2)
     #calculate
     magnitude_normal = ball1.abs_dis(ball2)
     x_normal = (ball1.x_pos - ball2.x_pos)/(magnitude_normal)
     y_normal = (ball1.y_pos - ball2.y_pos)/(magnitude_normal)
 
-    x_tangent = - 1 * y_normal
-    y_tangent = x_normal
+    x_tangent =  -1 * y_normal
+    y_tangent =  x_normal
 
     ball1_normal_x_speed = x_normal * ball1.x_speed
     ball1_normal_y_speed = y_normal * ball1.y_speed
@@ -105,23 +107,29 @@ def detect_wall_collision(ball)
   if ((ball.x_pos - BALL_RADIUS) <= 0  && ball.x_speed < 0 ) || 
      ((ball.x_pos + BALL_RADIUS) >= WIDTH && ball.x_speed > 0)
      ball.x_speed = ball.x_speed * -1
-     ball.collision = nil
+     if ball.collision != nil && ball.collision.collision == ball
+      ball.collision.collision = nil
+    end
+    ball.collision = nil
   end
   if ((ball.y_pos - BALL_RADIUS) <= 0 && ball.y_speed < 0) || 
     ((ball.y_pos + BALL_RADIUS) >= LENGTH && ball.y_speed > 0)
     ball.y_speed = ball.y_speed * -1
-    ball.collision = nil  
+    if ball.collision != nil && ball.collision.collision == ball
+      ball.collision.collision = nil
+    end
+    ball.collision = nil
   end
 end
 
 def detect_ball_collision(ball1,ball2,collision_array)
   if (ball1.abs_dis(ball2) <= 2 * BALL_RADIUS )
-    if(ball1.collision != ball2 )
-       sim_collision_with(ball1,ball2)
-        ball1.add_collsion(ball2)
+    if(ball1.collision != ball2 and ball2.collision != ball1)
+      sim_collision_with(ball1,ball2)
+      add_collision(ball1,ball2)
     end
-  else
-    ball1.remove_collision(ball2)
+  elsif ball1.collision == ball2 || ball2.collision == ball1
+    remove_collision(ball1,ball2)
   end
 end
 #################################################
@@ -129,16 +137,16 @@ end
 balls = []
 collision_array = []
 #balls << Ball.new("Violet" , 250, 0 , 0 ,5) 
-balls << Ball.new("red"   , 250, 400 , 0  , 0)
 #balls << Ball.new("blue"  , 5, 10  , VELOCITY/7  ,-2)
+balls << Ball.new("red"   , 250, 400 , 0  , 0)
 balls << Ball.new("pink"  , 75,  200  , VELOCITY/2  ,-3) 
 balls << Ball.new("yellow", 150, 550  , VELOCITY/4  ,-2)
 balls << Ball.new("black" , 200, 150  , 0           ,5)
 
 #NUM_BALLS.times do |x|
-  #color_num = rand(color_array.length - 1)
-  #balls << Ball.new("red",rand(WIDTH),rand(LENGTH),VELOCITY * rand ,rand * Math::PI)  
-  #color_array.slice!(color_num)
+#  color_num = rand(color_array.length - 1)
+#  balls << Ball.new("red",rand(WIDTH),rand(LENGTH),VELOCITY * rand ,rand * Math::PI)  
+#  color_array.slice!(color_num)
 #end
 
 
@@ -147,12 +155,14 @@ drawer = Drawer.new
 
 #Main Loop
 MAX_TIME.times do |time|
+  finish = true
   for i in 0...balls.length
     for j in i...balls.length
         detect_ball_collision(balls[i],balls[j],collision_array) if i != j
     end
     detect_wall_collision(balls[i])
     balls[i].move
+    finish = false if balls[i].x_speed != 0 || balls[i].y_speed !=0
   end
 
   #balls.each do |ball|
@@ -163,9 +173,11 @@ MAX_TIME.times do |time|
   #end
   
   drawer.draw(balls)
-  
+break if finish == true
 end
-
+balls.each do |ball|
+  puts ball
+end
 
 
 
